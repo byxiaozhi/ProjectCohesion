1150e44f69a047f90f21555637ffc07f9142c16b
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-03-29 01:07:05 +0800

    调整目录结构

---
+++ ProjectCohesion.Core/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCohesion.Core.ViewModels
+{
+    public abstract class ViewModelBase: INotifyPropertyChanged
+    {
+#pragma warning disable CS0067
+        // PropertyChanged.Fody 会自动监听属性变化并发出事件
+        public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0067
+    }
+}
--- ProjectCohesion/Windows/MainWindow.xaml.cs
+++ ProjectCohesion/Windows/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace ProjectCohesion
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
