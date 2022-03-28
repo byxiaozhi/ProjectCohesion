a6e18ccec4000021babcf75ba10cfa725fd420bd
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-03-29 00:05:44 +0800

    创建项目

---
+++ ProjectCohesion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectCohesion
+{
+    class Program
+    {
+        [STAThread()]
+        public static void Main()
+        {
+            using (new XamlHost.App())
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
