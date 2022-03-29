50e7d80200f22c4122ce846e82699565daa96d76
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2022-03-30 01:00:44 +0800

    完善主窗口部分布局

---
+++ ProjectCohesion.Win32/Controls/BasicControls/TabView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectCohesion.Win32.Controls.BasicControls
+{
+    public partial class TabView : UserControl
+    {
+        public TabView()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
